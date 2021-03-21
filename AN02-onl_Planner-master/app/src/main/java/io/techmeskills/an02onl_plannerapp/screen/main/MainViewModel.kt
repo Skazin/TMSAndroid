60c435218a344c950ca1b5a1e67427ae7c28e254
Skazin <van00746@gmail.com>
2021-03-21 19:47:42 +0300

    HomeWork 2

--- AN02-onl_Planner-master/app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
+++ AN02-onl_Planner-master/app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
@@ -3,4 +3,21 @@ package io.techmeskills.an02onl_plannerapp.screen.main
 import io.techmeskills.an02onl_plannerapp.support.CoroutineViewModel
 
 class MainViewModel : CoroutineViewModel() {
+
+    val notes = listOf(
+        Note("Амброксол", "Отхаркивающее"),
+        Note("Диклофенак", "НПВС"),
+        Note("Кетопрофен", "НПВС"),
+        Note("Ацетилцистеин", "Отхаркивающее"),
+        Note("Фенибут", "Анксиолитик"),
+        Note("Валерианы настойка"),
+        Note("Вода питьевая"),
+        Note("Крем детский"),
+    )
+
 }
+
+class Note(
+    val title: String,
+    val group: String? = null
+)
